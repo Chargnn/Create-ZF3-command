3d2dd0721dff2b5c6a2bffa0d913a8f536d6d3d6
Alexis Coulombe <alexis.410@hotmail.com>
2020-03-26 08:34:10 -0400

    Create your-command.php

---
+++ your-command.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Application\Controller\LoginController;
+use Symfony\Component\Console\Application;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Mvc\Application as ZendApplication;
+
+$appConfig = require __DIR__ . '/../config/application.config.php';
+if (file_exists(__DIR__ . '/../config/development.config.php')) {
+    $appConfig = ArrayUtils::merge(
+        $appConfig,
+        require __DIR__ . '/../config/development.config.php'
+    );
+}
+$zendApplication = ZendApplication::init($appConfig);
+$serviceManager = $zendApplication->getServiceManager();
+
+(new Application('Name of your application'))
+    ->register('your-command-name')
+    ->setCode(
+        function () use ($serviceManager) {
+
+            /** @var LoginController $controller */
+            $controller = $serviceManager->get('ControllerManager')->get(YourController::class);
+
+            echo 'Running YourControler->commandToRun()' . PHP_EOL;
+            $controller->commandToRun();
+        }
+    )
+    ->getApplication()
+    ->run();
